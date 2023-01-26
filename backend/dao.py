fd9eaa80f3255fba4c9be2f44e1c2fc4a3c9a985
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-01-26 16:43:03 +0530

    backend/: add logic

--- backend/dao.py
+++ backend/dao.py
@@ -1,33 +1,86 @@
 import sqlite3
-from contextlib import contextmanager
+from threading import Lock
 
 
+# Commit the transaction on success, rollback on failure + thread safety
 class Ensure:
-    def __init__(self, conn):
+    def __init__(self, conn, lock):
         self.conn = conn
+        self.lock = lock
+
+        self.lock.acquire()
+
+    def close(self):
+        self.conn.close()
 
     def __enter__(self):
         return self.conn
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type:
-            self.conn.rollback()
-        else:
-            self.conn.commit()
+        try:
+            if exc_type:
+                self.conn.rollback()
+            else:
+                self.conn.commit()
+        except Exception:
+            self.lock.release()
+            raise
 
 
 class Database:
     def __init__(self, db_path):
-        self.conn = sqlite3.connect(db_path)
-        self.conn.row_factory = sqlite3.Row
+        conn = sqlite3.connect(db_path, check_same_thread=False)
+        conn.row_factory = sqlite3.Row
 
-        self.cur = self.conn.cursor()
+        self.ensure = Ensure(conn, Lock())
+        self.cur = conn.cursor()
 
         self._create()
 
     def _create(self):
-        with Ensure(self.conn):
-            ...
+        with self.ensure:
+            self.cur.execute(
+                "CREATE TABLE IF NOT EXISTS Users"
+                "(discord_id TEXT UNIQUE NOT NULL PRIMARY KEY, psn_id TEXT NOT NULL)"
+            )
+
+            self.cur.execute(
+                "CREATE TABLE IF NOT EXISTS Trophies"
+                "(discord_id FOREIGN KEY, total INT, platinum INT, gold INT, silver INT, bronze INT)"
+            )
+
+    def set_id_to_psn(self, discord_id, psn_id):
+        with self.ensure:
+            self.cur.execute(
+                "INSERT OR REPLACE INTO Users VALUES((?), (?))",
+                discord_id,
+                psn_id,
+            )
+
+    def get_id_to_psn_batch(self):
+        with self.ensure:
+            self.cur.execute("SELECT discord_id, psn_id FROM Users")
+
+            return {
+                result["discord_id"]: result["psn_id"]
+                for result in self.cur.fetchall()
+            }
+
+    def set_id_to_trophies_batch(self, id_to_trophies):
+        with self.ensure:
+            for id, trophies in id_to_trophies.items():
+                self.cur.execute(
+                    "UPDATE Trophies SET total = (?), platinum = (?), gold = (?), silver = (?), bronze = (?)"
+                    "WHERE discord_id = (?)",
+                    (
+                        trophies["total"],
+                        trophies["platinum"],
+                        trophies["gold"],
+                        trophies["silver"],
+                        trophies["bronze"],
+                        id,
+                    ),
+                )
 
     def close(self):
-        self.conn.close()
+        self.ensure.close()
--- backend/scrape_worker.py
+++ backend/scrape_worker.py
@@ -1,12 +1,59 @@
-import time
+from dataclasses import asdict
+from queue import Empty, Queue
+from time import sleep, time
+from typing import Callable
 
 import lib.discord as discord
-import lib.roles as roles
+from lib.roles import Roles
+from lib.scraper import fetch_trophies
 
 
-def run(token, refresh_interval_hours, roles_threshold):
+def scrape(id_to_psn):
+    id_to_trophies = {}
+
+    for id, psn in id_to_psn.items():
+        # Convert the dataclass to a dict: {"total": ..., "gold": ...}
+        id_to_trophies[id] = asdict(fetch_trophies(psn))
+        sleep(1)  # TODO is this enough for rate limiting?
+
+    return id_to_trophies
+
+
+def run(
+    token: str,
+    refresh_interval_hours: int,
+    roles_threshold: dict,
+    guilds: list,
+    instant_update_queue: Queue,
+    id_to_psn_batch_get: Callable,
+    id_to_trophies_batch_set: Callable,
+):
     api = discord.API(token, bot=True)
-    roles_syncer = roles.Roles(api, roles_threshold)
+    roles_syncer = Roles(api, roles_threshold)
+
+    def sync(id_to_trophies):
+        for guild in guilds:
+            roles_syncer.sync_roles(guild, id_to_trophies)
+
+    start_time = time()
 
     while True:
-        time.sleep(refresh_interval_hours * 60 * 60)
+        try:
+            instant_id, instant_psn = instant_update_queue.get(block=False)
+            id_to_trophies = scrape({instant_id: instant_psn})
+
+            id_to_trophies_batch_set(id_to_trophies)
+            sync(id_to_trophies)
+        except Empty:
+            pass
+
+        sleep(10)
+
+        # Time elapsed (in seconds) >= interval (in seconds)
+        if (time() - start_time) >= (refresh_interval_hours * 60 * 60):
+            start_time = time()
+
+            id_to_trophies = scrape(id_to_psn_batch_get())
+
+            id_to_trophies_batch_set(id_to_trophies)
+            sync(id_to_trophies)
